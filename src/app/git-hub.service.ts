8b33ed5b0b7791d8aa4563e994de3eea1ca6eff0
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-12 21:39:41 +0200

    Agrega dependencia @octokit/core y método getFavoriteRepos a GitHubService

--- src/app/git-hub.service.ts
+++ src/app/git-hub.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Octokit } from "@octokit/core";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitHubService {
+  octokit: Octokit = new Octokit({
+    auth: "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"
+  });
 
-  constructor() { }
+  async getFavoriteRepos() {
+    try {
+      const response = await this.octokit.request('GET /users/{username}/starred', {
+        username: 'XxFenixDCxX'
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error al obtener los repositorios favoritos:", error);
+      throw error;
+    }
+  }
 }

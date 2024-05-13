f39fe56d548e1f66c04e07cb16b4f2de7b1b6609
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-13 18:53:54 +0200

    Corregir tipo de parámetro en el evento onRightClick()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
 
   @HostListener('contextmenu', ['$event'])
   @HostListener('keydown.F12', ['$event'])
-  onRightClick(event: MouseEvent) {
+  onRightClick(event: any) {
     event.preventDefault();
     const alertDiv = document.createElement('div');
     alertDiv.classList.add('fixed', 'top-1/2', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2', 'bg-red-100', 'text-red-600', 'px-4', 'py-2', 'border', 'border-red-400', 'rounded', 'shadow-md', 'z-50');

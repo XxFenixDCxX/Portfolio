bbf386430d1e6f4846694a4e9c1bf5f36d269c85
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-13 18:58:13 +0200

    Eliminar código innecesario de manejo de clic derecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,19 +15,4 @@ import { FooterComponent } from './footer/footer.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor() {}
-
-  @HostListener('contextmenu', ['$event'])
-  @HostListener('keydown.F12', ['$event'])
-  onRightClick(event: any) {
-    event.preventDefault();
-    const alertDiv = document.createElement('div');
-    alertDiv.classList.add('fixed', 'top-1/2', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2', 'bg-red-100', 'text-red-600', 'px-4', 'py-2', 'border', 'border-red-400', 'rounded', 'shadow-md', 'z-50');
-    alertDiv.innerHTML = '<strong>¡Oops!</strong> La inspección del código está deshabilitada.';
-    document.body.appendChild(alertDiv);
-
-    setTimeout(() => {
-      alertDiv.remove();
-    }, 2000);
-  }
 }

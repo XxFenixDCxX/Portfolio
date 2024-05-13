d42fa860d3a0b9b3a3ade58559956f4d2e102b1a
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-13 18:45:47 +0200

    Agregado manejo de clic derecho en AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -15,4 +15,18 @@ import { FooterComponent } from './footer/footer.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor() {}
+
+  @HostListener('contextmenu', ['$event'])
+  onRightClick(event: MouseEvent) {
+    event.preventDefault();
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('fixed', 'top-1/2', 'left-1/2', 'transform', '-translate-x-1/2', '-translate-y-1/2', 'bg-red-100', 'text-red-600', 'px-4', 'py-2', 'border', 'border-red-400', 'rounded', 'shadow-md', 'z-50');
+    alertDiv.innerHTML = '<strong>¡Oops!</strong> La inspección del código está deshabilitada.';
+    document.body.appendChild(alertDiv);
+
+    setTimeout(() => {
+      alertDiv.remove();
+    }, 2000);
+  }
 }

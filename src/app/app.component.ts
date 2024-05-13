da5ff4a163e2e46b2ab2ce6228cba51b40892e99
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-13 17:19:21 +0200

    Agregado componente de pie de página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { AboutComponent } from './about/about.component';
 import { ProyectsComponent } from './proyects/proyects.component';
 import { KnowledgeComponent } from './knowledge/knowledge.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HomeComponent, AboutComponent, ProyectsComponent, KnowledgeComponent, ContactComponent],
+  imports: [NavbarComponent, HomeComponent, AboutComponent, ProyectsComponent, KnowledgeComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

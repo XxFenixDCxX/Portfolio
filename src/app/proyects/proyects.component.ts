9c994ea13138075308d578e8dcea228a7f796f67
XxFenixDCxX <fenixdc7@gmail.com>
2024-05-14 14:06:57 +0200

    Terminadas traducciones con seccion de proyectos

--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GitHubService } from '../git-hub.service';
 import { CommonModule } from '@angular/common';
+import { TranslationService } from '../translation.service';
 
 @Component({
   selector: 'app-proyects',
@@ -13,7 +14,7 @@ export class ProyectsComponent implements OnInit{
   repos: any[] = [];
   reposErro: boolean = false;
 
-  constructor(private gitHubService: GitHubService) { }
+  constructor(private gitHubService: GitHubService, private translator: TranslationService) { }
 
   ngOnInit(): void {
     this.gitHubService.getFavoriteRepos().then((repos) => {
@@ -31,5 +32,10 @@ export class ProyectsComponent implements OnInit{
 
   isMobile() {
     return window.innerWidth <= 825;
+  }
+
+
+  translate(key: string): string {
+    return this.translator.translate(key);
   }
 }
